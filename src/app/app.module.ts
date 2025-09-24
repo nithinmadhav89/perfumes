b8e160c8b2ed24bd73e408777ac3ec98e2496cfa
nithinmadhav <nithinmadhav89@gmail.com>
2025-09-24 11:53:24 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductFilterComponent } from './components/product-filter/product-filter.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductCardComponent,
+    ProductFilterComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
